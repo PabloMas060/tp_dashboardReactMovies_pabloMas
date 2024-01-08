0f8d5fbe80c511b14877ca80c364f239213f50fd
PabloMas060 <pablo.mas.dev@gmail.com>
2024-01-08 11:13:18 -0300

    subo tp react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './assets/css/app.css'
+import { ContentWrapper } from './components/ContentWrapper'
+import { Sidebar } from './components/Sidebar'
+
+function App() {
+
+  return (
+    <div id="wrapper">
+
+<Sidebar/>
+<ContentWrapper/>
+
+    </div>
+  )
+}
+
+export default App
